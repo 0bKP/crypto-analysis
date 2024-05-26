74c062c1a8d1e6cf9129be86b00b4835661899bd
0bKP <kacper.pajor42@gmail.com>
2024-05-26 17:41:44 +0200

    clear_chart, toggle and stuff

--- src/gui.py
+++ src/gui.py
@@ -13,11 +13,11 @@ class App(tk.Tk):
         self.resizable(False, False)
 
         # Nav bar
-        self.nav_bar = tk.Frame(background=logoBarColor, height=100, highlightbackground="red", highlightthickness=1)
+        self.nav_bar = tk.Frame(background=logoBarColor, height=100)
         self.nav_bar.pack(side="top", fill="x", padx=10, pady=10)
         self.nav_bar.pack_propagate(False)
 
-        # # # Buttons
+        # # # Nav bar buttons
         self.burger_button = tk.Button(self.nav_bar, width=8, text="Burger", command=self.toggle_settings_bar)
         self.burger_button.pack(side="left", fill="y")
 
@@ -25,28 +25,41 @@ class App(tk.Tk):
         self.analysis_button.pack(side="top")
 
         # Main screen
-        self.display_screen = tk.Frame(self, background=bgColor, width=220, highlightbackground="green",
-                                       highlightthickness=1)
+        self.display_screen = tk.Frame(self, background=bgColor, width=220)
         self.display_screen.pack(side="right", expand=True, fill="both", padx=10, pady=10)
         self.display_screen.pack_propagate(False)
 
-        self.plot_frame = tk.Frame(self.display_screen, background=bgColor, highlightbackground="blue", highlightthickness=1,
-                                   width=100, height=100)
-        self.plot_frame.pack(side="top", anchor="nw")
+        # # # Plot
+        self.plot_frame = tk.Frame(self.display_screen, background=bgColor, width=100, height=100)
+        self.plot_frame.pack(side="left", anchor="nw")
 
-        # Settings bar
-        self.settings_bar = tk.Frame(self, background=bgColor, width=70, height=100, highlightbackground=logoBarColor,
-                                     highlightthickness=1)
+        # # # Analysis tools frame
+        self.analysis_tools_frame = tk.Frame(self.display_screen, background=bgColor, width=100, height=100,
+                                             highlightthickness=1, highlightbackground="pink")
+        self.analysis_tools_frame.propagate(False)
 
-        self.exit = tk.Button(self.display_screen, text='X', command=self.destroy)
-        self.exit.pack(side="right")
+        self.analysis_label = tk.Label(self.analysis_tools_frame, text="Analysis")
+        self.analysis_label.pack(side="top", fill="x")
+
+        self.cckbtn = tk.IntVar()
+        self.trend_line = tk.Checkbutton(self.analysis_tools_frame, text="Trend line", variable=self.cckbtn,
+                                         onvalue=1, offvalue=0,
+                                         command=lambda: data_processing.DataProcessing.trend_line(self.cckbtn.get()))
+        self.trend_line.pack(side="right", padx=10, pady=10)
+
+
+        # # # Another frame idk what for yet
+        self.bottom_frame = tk.Frame(self.plot_frame, background=bgColor, width=100, height=100, highlightthickness=1,
+                                     highlightbackground="blue")
+        self.bottom_frame.pack(side="bottom", expand=True, fill="both")
+        self.bottom_frame.propagate(False)
 
         # Settings bar
         self.settings_bar = tk.Frame(self, background=bgColor, width=100, height=100, highlightbackground=logoBarColor,
                                      highlightthickness=1)
         self.settings_bar.propagate(False)
 
-        # # # Options
+        # # # Settings bar options
         # What stock to use
         self.stock_var = tk.StringVar(self)
         self.stock_var.set("Binance")
@@ -58,25 +71,35 @@ class App(tk.Tk):
         # Which symbol to use
         self.symbol_var = tk.StringVar(self)
         self.symbol_var.set("BTC/USDT")
-        self.symbol = tk.OptionMenu(self.settings_bar, self.symbol_var, "BTC/USDT", "ETH/USDT")
+        self.symbol = tk.OptionMenu(self.settings_bar, self.symbol_var, "BTC/USDT", "ETH/USDT", "DOGE/USDT",
+                                    "BTC/PLN")
         self.symbol.config(background=bgColor, highlightthickness=0, foreground="white", highlightcolor=bgColor,
                            borderwidth=0)
         self.symbol.pack(side="top")
 
-        self.run = tk.Button(self.settings_bar, text="Analyze!", background="green", foreground="white",
+        self.run = tk.Button(self.settings_bar, text="Analyze!", background="#1DAF1A", foreground="white",
+                             height=5,
                              command=lambda: self.run_analysis(self.stock_var.get(), self.symbol_var.get()))
-        self.run.pack(side="bottom")
+        self.run.pack(side="bottom", fill="x")
+
+    def toggle(self, element, side, expand=False, fill=None, padx=0, pady=0):
+        if element.winfo_ismapped():
+            element.pack_forget()
+        else:
+            element.pack(side=side, expand=expand, fill=fill, padx=padx, pady=pady)
 
     def toggle_settings_bar(self):
-        if self.settings_bar.winfo_ismapped():
-            self.settings_bar.pack_forget()
-        else:
-            self.settings_bar.pack(side="left", fill="y", padx=10, pady=10)
+        self.toggle(self.settings_bar, "left", False, "y", 10, 10)
 
-    def analysis_action(self): pass
+    def analysis_action(self):
+        self.toggle(self.analysis_tools_frame, "right", True, "both", 0, 0)
 
     def run_analysis(self, stock, symbol):
-        data_processing.DataProcessing(self.plot_frame, stock, symbol)
+        if not hasattr(self, 'chart'):
+            self.chart = data_processing.DataProcessing(self.plot_frame, stock, symbol)
+        else:
+            self.chart.clear_chart()
+            self.chart = data_processing.DataProcessing(self.plot_frame, stock, symbol)
 
 
 if __name__ == '__main__':
